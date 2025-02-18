d89ef7ffa40ed10260416330d986668061e175b9
beckym223 <becky.marcu23@gmail.com>
2025-02-18 19:44:27 +0000

    Made script to do searchKs with all the params

---
+++ SearchingKs.R
@@ -0,0 +1,57 @@
+library(furrr)   # For parallelized processing
+library(dplyr)   # For data manipulation
+library(readr)   # For saving CSVs
+library(stm)
+library(tibble)  # Creating tidy dataframes
+library(stm) 
+# Set up parallel execution with 8 cores
+
+results_metadata <- read_csv("~/R_Work/results/results_metadata.csv")
+get_topic_models <- function(id,
+                             seed = 632,
+                             starter_ks = c(10, 20, 40, 50, 60, 70, 80, 100)) {
+  
+  # Set a unique seed for this iteration
+  set.seed(seed + as.integer(as.factor(id)))
+  
+  # Load the saved object for this param_id
+  object_path <- results_metadata %>%
+    filter(param_id == id) %>%
+    pull(file_path)
+  
+  object <- readRDS(object_path)
+  results <- object$results
+  raw_documents <- results$documents
+  stm <- results$dfm2stm
+  docs <- stm$documents
+  vocab <- stm$vocab
+  data <- stm$meta
+  
+  # Run searchK without specifying cores
+  search_output <- searchK(docs, vocab, starter_ks, prevalence=~s(year), data=data)
+  
+  # Convert results to numeric and add param_id
+  kresults <- search_output$results %>%
+    mutate(across(everything(), as.numeric)) %>%
+    mutate(param_id = id)
+  
+  return(kresults)
+}
+
+
+
+n_cores = as.integer(availableCores())
+plan(multisession, workers = n_cores)
+
+# Run topic modeling in parallel
+topic_model_results <- future_map_dfr(results_metadata$param_id, get_topic_models)
+
+metadata_to_save <- results_metadata %>%
+  select(!c(file_path,last_modified,params_json))
+# Merge topic model results with corresponding parameters
+final_results <- topic_model_results %>%
+  left_join(metadata_to_save,
+            by = "param_id")
+
+# Save the merged dataframe as CSV and RDS
+write_csv(final_results, "~/R_Work/results/topic_model_results_with_params.csv")
