38c04fda0643cc2460e43fd03a0f358de2c0fea5
beckym223 <becky.marcu23@gmail.com>
2025-02-19 18:54:54 +0000

    Added progress

--- SearchingKs.R
+++ SearchingKs.R
@@ -4,14 +4,15 @@ library(readr)   # For saving CSVs
 library(stm)
 library(tibble)  # Creating tidy dataframes
 library(stm) 
+library(progressr)
 # Set up parallel execution with 8 cores
 
 results_metadata <- read_csv("~/R_Work/results/results_metadata.csv")%>%
   filter(split_on=="paragraph")
-furrr_options(seed = TRUE)
+
 get_topic_models <- function(id,
                              seed,
-                             starter_ks = c(10, 20, 40, 50, 60, 70, 80, 100)) {
+                             starter_ks = c(10,20,40,50,60,70,80)) {
   
   # Set a unique seed for this iteration
   set.seed(seed + as.integer(as.factor(id)))
@@ -51,15 +52,22 @@ n_cores = as.integer(availableCores())
 plan(multisession, workers = n_cores)
 
 param_ids = results_metadata$param_id
-results_df <- future_map_dfr(
-  param_ids,
-  ~ get_topic_models(.x, seed = 632),  # Pass a base seed
-  .options = furrr_options(seed = TRUE)  # Ensures safe parallel RNG
-)
+with_progress({
+  p <- progressor(steps = length(param_ids))
+  results_df <- future_map_dfr(
+    param_ids,
+    ~ {p()
+      Sys.sleep(.2)
+      get_topic_models(.x, seed = 632)},  # Pass a base seed
+    .options = furrr_options(seed = TRUE)  # Ensures safe parallel RNG
+  )
+  
+})
+
 metadata_to_save <- results_metadata %>%
   select(!c(file_path,last_modified,params_json))
 # Merge topic model results with corresponding parameters
-final_results <- topic_model_results %>%
+final_results <- results_df %>%
   left_join(metadata_to_save,
             by = "param_id")
 
